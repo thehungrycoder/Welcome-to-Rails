d3f758c47e5a887a25b5ba866fa1120beca58224
HungryCoder <rajuru@gmail.com>
2012-12-24 09:18:25 +0600

    initial commit after scaffolding post and comment resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3c3d913c259d063baa1db20db5584084904b1a0c5cc1dcf25b49625c58b1f56ab885db903a1d3b79c96e8bc6b7d4e9896fe70268e994d260bf9ca965d8030950'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Blog::Application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+end
